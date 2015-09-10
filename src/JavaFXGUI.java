46b9a88ab4ec2cdfb19935868ceb0134d8f402f7
Archimald <michaelcurrie12@augustana.edu>
2015-09-10 01:12:03 -0500

    add JavaFXGUI class with JavaFX files for FX version of the UI.

---
+++ src/JavaFXGUI.java
@@ -0,0 +1,28 @@
+
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class JavaFXGUI extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = FXMLLoader.load(getClass().getResource("potteryGUI.fxml"));
+			Scene scene = new Scene(root,640,480);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Pottery GUI");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
