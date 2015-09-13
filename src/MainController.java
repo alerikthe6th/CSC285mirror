5a707efd1230e6c2643ce16c31d993f65b1ecf72
michaelcurrie12 <michaelcurrie12@augustana.edu>
2015-09-13 18:36:54 -0500

    add options to combobox

--- src/MainController.java
+++ src/MainController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +14,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
@@ -21,8 +24,13 @@ public class MainController implements Initializable{
 	@FXML
 	private Button newOrderButton;
 	private MainApp mainApp;
+	@FXML
+	private ComboBox<String> cmbOrderStatus;
+	
 	
 	public MainController() {
+		
+
 		
 	}
 	
@@ -45,6 +53,17 @@ public class MainController implements Initializable{
 	            ex.printStackTrace();
 	        }
 	    }
+	    
+	    private void populateOrderStatus() {
+	    	
+	    ObservableList<String> options = 
+	    	    FXCollections.observableArrayList(
+	    	        "Option 1",
+	    	        "Option 2",
+	    	        "Option 3"
+	    	    );
+	    	cmbOrderStatus.setItems(options);
+	    }
 	          
 	    	
 	    	
@@ -57,6 +76,8 @@ public class MainController implements Initializable{
 		@Override
 		public void initialize(URL arg0, ResourceBundle arg1) {
 			assert newOrderButton != null : "fx:id=\"newOrderButton\" was not injected: check your FXML file 'potteryGUI.fxml'.";
+			assert cmbOrderStatus != null : "fx:id=\"newOrderButton\" was not injected: check your FXML file 'potteryGUI.fxml'.";
+			populateOrderStatus();
 			
 		}
 		
