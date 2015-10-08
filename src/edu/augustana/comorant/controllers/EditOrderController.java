62fda5eac3827e052985e79b9ee5426b53e2dd17
Archimald <michaelcurrie12@augustana.edu>
2015-10-07 21:28:36 -0500

    fixed empty states list on edit order

--- src/edu/augustana/comorant/controllers/EditOrderController.java
+++ src/edu/augustana/comorant/controllers/EditOrderController.java
@@ -328,6 +328,15 @@ public class EditOrderController implements Initializable {
 		// set payment method
 		ObservableList<String> contactOptions = FXCollections.observableArrayList("Email", "Phone", "Text");
 		cmbPrefContactMethod.setItems(contactOptions);
+		
+		ObservableList<String> statesList = FXCollections.observableArrayList("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+				"Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+				"Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
+				"Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+				"New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
+				"Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas",
+				"Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming");
+		cmbState.setItems(statesList);
 
 	}
 
