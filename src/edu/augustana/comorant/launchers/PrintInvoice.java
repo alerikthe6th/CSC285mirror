facb331d5bdf5e001c665d1c6285060f7df798dd
josephgodfrey14 <josephgodfrey14@augustana.edu>
2015-10-15 02:38:23 -0500

    printInvoice sets "date shipped" to current date instead of taking it as a parameter. Also javadoc'd the methods.

--- src/edu/augustana/comorant/launchers/PrintInvoice.java
+++ src/edu/augustana/comorant/launchers/PrintInvoice.java
@@ -5,6 +5,9 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.print.DocFlavor;
 import javax.print.DocPrintJob;
@@ -18,46 +21,67 @@ import javax.print.SimpleDoc;
 //printer class will take as parameters the following: from address(from preferences), to address(from customer), 
 //date ordered, date shipped, order desc, price, payment method (from order)
 
-//printpage and createinvoice should not be static
-
 //will date ordered and date shipped be strings?
 public class PrintInvoice {
 	/*
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException{
-		createInvoice("C:/Users/josephgodfrey14/Desktop/PrintOut.doc", "Firsty Lastname", "123 Main Street", "New York, NY 12345", 
-				"John Cena", "123 JCena St.", "Cena WY, 54321", "Oct 13 2015", "Oct 14 2015", "12 Pots, 6 Plates, and one giant hug", 
+		createInvoice("C:/Users/Joseph/Desktop/PrintOut.doc", "Firsty Lastname", "123 Main Street", "New York, NY 12345",
+		//createInvoice("C:/Users/josephgodfrey14/Desktop/PrintOut.doc", "Firsty Lastname", "123 Main Street", "New York, NY 12345", 
+				"John Cena", "123 JCena St.", "Cena WY, 54321", "Oct 13 2015", "12 Pots, 6 Plates, and one giant hug", 
 				12.56, "Credit Card");
-		printPage("C:/Users/josephgodfrey14/Desktop/PrintOut.doc");
+		//printPage("C:/Users/josephgodfrey14/Desktop/PrintOut.doc");
 	}
 	*/
-	
+	/**
+	 * 
+	 * @param saveToDesktopString - String; ex: "C:/Users/Joseph/Desktop/PrintOut.doc"
+	 * @param fromName - String; ex: "Firsty Lastname"
+	 * @param fromAdrs - String; ex: "123 Main Street"
+	 * @param fromCSZ - String; ex: "New York, NY 12345"
+	 * @param custName - String; ex: "John Q. Customer"
+	 * @param custAdrs - String; ex: "1755 John Cena Boulevard"
+	 * @param custCSZ - String; ex: "New York, NY 12345"
+	 * @param dateOrdered - String; ex: "October 12, 2015"
+	 * @param dateShipped - String; ex: "October 12, 2015"
+	 * @param orderDesc - String; ex: "12 Pots, 6 Plates, and a big hug"
+	 * @param price - double; ex: 12.34
+	 * @param paymentMethod - String; ex: "Credit Card"
+	 * @throws FileNotFoundException only thrown if the filepath is incorrect
+	 * @throws UnsupportedEncodingException when the URL(filepath) of the file has invalid characters
+	 */
 	protected void createInvoice(String saveToDesktopString, String fromName, String fromAdrs, String fromCSZ, String custName, String custAdrs,
-		String custCSZ, String dateOrdered, String dateShipped, String orderDesc, double price, String paymentMethod)
+		String custCSZ, String dateOrdered, String orderDesc, double price, String paymentMethod)
 		throws FileNotFoundException, UnsupportedEncodingException{
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd/MMMM/yyyy");
+		Date date = new Date();
+		String currentDate = (dateFormat.format(date)).toString(); //10/15/2015
 		
 		PrintWriter writer = new PrintWriter(saveToDesktopString, "UTF-8");
 
 		String lines="-----------------------------------";//35 chars - print twice for 70 wide line (easier than 7*10)
 		
 
-		writer.printf(fromName+"%"+(70-fromName.length())+"s", "+------+");
+		writer.printf(fromName+"%"+(70-fromName.length())+"s", "+------+");//don't use \n here: it messes up the formatting
 		writer.println();
-		writer.printf(fromAdrs+"%"+(70-fromAdrs.length())+"s", "+-stamp+");
+		writer.printf(fromAdrs+"%"+(70-fromAdrs.length())+"s", "+-stamp+");//or here
 		writer.println();
-		writer.printf(fromCSZ+"%"+(70-fromCSZ.length())+"s", "+------+");
+		writer.printf(fromCSZ+"%"+(70-fromCSZ.length())+"s", "+------+");//or here either
 		writer.println();
 		
-		writer.println("\n\n\n\n");
+		writer.println("\n\n\n\n");//5 return
 	
 		writer.println("\t\t\t\t\t"+custName);
 		writer.println("\t\t\t\t\t"+custAdrs);
 		writer.println("\t\t\t\t\t"+custCSZ+"\n");
 				
-		writer.println("\n\n\n\n"+lines+lines+"\n\n\n\n");
+		writer.println("\n\n\n\n"+lines+lines+"\n\n\n\n");//4 returns, a horizontal line and then 5 returns
 		
 		writer.println("Order Details:\n");
 		
-		writer.println("\tDate Ordered:\t" +dateOrdered+"\t\tDate Shipped:\t"+dateShipped +"\n\n");
+//		writer.println("\tDate Ordered:\t" +dateOrdered+"\t\tDate Shipped:\t"+dateShipped +"\n\n");
+		writer.println("\tDate Ordered:\t" +dateOrdered+"\t\tDate Shipped:\t"+currentDate +"\n\n");
+		writer.println("\tDate Ordered:\t" +dateOrdered+"\t\tDate Shipped:\t"+currentDate +"\n\n");
 		writer.println("\tItems Ordered:\n\t" +orderDesc +"\n\n\n");
 		writer.println("\tPaid With:\n\t" +paymentMethod+"\n");
 		
@@ -69,6 +93,11 @@ public class PrintInvoice {
 	//used code from: http://www.coderanch.com/t/410208/java/java/java-printing-printing-pdf
 	
 	//DO NOT TEST - LOADS PAPER BUT DOES NOTHING WITH IT
+	//EDIT: prints fine in olin - Al can attest
+	/**
+	 * Takes in a filepath and name (in one string) and prints the target file
+	 * @param filePathAndName - String; ex: "C:/Users/Joseph/Desktop/PrintOut.doc"
+	 */
 	protected void printPage(String filePathAndName){
 		PrintService defaultPrintService = PrintServiceLookup.lookupDefaultPrintService();
 	    DocPrintJob printerJob = defaultPrintService.createPrintJob();
