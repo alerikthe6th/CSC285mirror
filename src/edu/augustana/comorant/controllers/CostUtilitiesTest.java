de6aa0eee43199d847c44c3a8861744f9ea6862b
Scott Doberstein <scottdoberstein12@augustana.edu>
2015-11-03 18:12:56 -0600

    Added two new JUnit tests

--- src/edu/augustana/comorant/controllers/CostUtilitiesTest.java
+++ src/edu/augustana/comorant/controllers/CostUtilitiesTest.java
@@ -14,5 +14,24 @@ public class CostUtilitiesTest {
 		assertEquals(totalCost, "58.8");
 
 	}
+	@Test
+	public void testCostUtilitiesNegative() {
+		double evaluatedCost = CostUtilities.getCost("10*5+6*-1");
+		double costIncludingTax = CostUtilities.getCostIncludingTax(evaluatedCost, 0.00);
+		String totalCost = "" + costIncludingTax;
+		assertEquals(totalCost, "44.0");
+
+	}
+	
+	@Test
+	public void testCostUtilitiesTax() {
+		double evaluatedCost = CostUtilities.getCost("10*5+6*1");
+		double costIncludingTax = CostUtilities.getCostIncludingTax(evaluatedCost, 0.50);
+		double tax = costIncludingTax - evaluatedCost;
+		String strTax = "" + tax;
+		assertEquals(strTax, "28.0");
+
+	}
+	
 
 }
